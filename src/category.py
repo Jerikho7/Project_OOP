18ad4ed03d01a7c1e84ca07ee348b3d9d4a6d151
Evgeniya <osipova.dr17@yandex.ru>
2025-01-11 14:20:09 +1100

    private products and new method add_product

--- src/category.py
+++ src/category.py
@@ -1,3 +1,4 @@
+from src.product import Product
 
 
 class Category:
@@ -11,6 +12,18 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.__products = products if products else []
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
+
+    def add_product(self, product):
+        """
+        Добавляет продукт в приватный список __products и увеличивает счетчик продуктов.
+        Аргумент:
+        product (Product): объект класса Product.
+        """
+        if isinstance(product, Product):
+            self.__products.append(product)
+            Category.product_count += 1
+        else:
+            raise ValueError("Only instances of Product can be added.")
