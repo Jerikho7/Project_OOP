2eb265a2143554feb1ad7d98aefff5bb2bdc5193
Evgeniya <osipova.dr17@yandex.ru>
2025-01-11 14:28:52 +1100

    add classmethod new_product

--- src/product.py
+++ src/product.py
@@ -11,3 +11,20 @@ class Product:
         self.description = description
         self.price = price
         self.quantity = quantity
+
+    @classmethod
+    def new_product(cls, product_data):
+        """
+        Создает новый экземпляр Product на основе данных словаря.
+        Аргументы:
+            product_data (dict): словарь с данными о продукте.
+                Ожидаются ключи: "name", "description", "price", "quantity".
+        Возвращает:
+                Product: экземпляр класса Product.
+        """
+        return cls(
+            name=product_data["name"],
+            description=product_data["description"],
+            price=product_data["price"],
+            quantity=product_data["quantity"],
+        )
